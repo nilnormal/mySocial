477f48afbe462246284790a01cf55894f6456160
xannas <wizwristy@gmail.com>
2017-07-17 17:39:48 +0700

    facebook auth complete

--- Podfile
+++ Podfile
@@ -10,5 +10,10 @@ target 'devslopes-social' do
   pod 'Firebase/Auth'
   pod 'Firebase/Database'
   pod 'Firebase/Storage'
-
+  pod 'FacebookCore', '~> 0.2'
+  pod 'FacebookLogin', '~> 0.2'
+  pod 'FacebookShare', '~> 0.2'
+  pod 'FBSDKCoreKit', '~> 4.22.1'
+  pod 'FBSDKLoginKit', '~> 4.22.1'
+  pod 'FBSDKShareKit', '~> 4.22.1'
 end
--- devslopes-social/SignInVC.swift
+++ devslopes-social/SignInVC.swift
@@ -7,6 +7,10 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
+
 
 class SignInVC: UIViewController {
 
@@ -20,6 +24,29 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func facebookBtnPressed(_ sender: Any) {
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("Unable to authenticate with Facebook - \(error)")
+            }else if result?.isCancelled == true{
+                print("User cancelled Facebook authenticate")
+            }else{
+                print("Successfully authenticate with Facebook")
+                let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential: credential)
+            }
+        }
+    }
+    func firebaseAuth(credential: AuthCredential){
+        Auth.auth().signIn(with: credential) { (user, error) in
+            if error != nil{
+                print("Unable to authenticate with Firebase -\(error)")
+            }else{
+                print("Successfully authenticate with Firebase")
+            }
+        }
+    }
 
 }
 
