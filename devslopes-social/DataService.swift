0919b85fd4b703b1b8d4cf9d918d5ef1cdec9959
xannas <wizwristy@gmail.com>
2017-07-18 17:41:25 +0700

    Complete Firebase storeage download

--- devslopes-social/DataService.swift
+++ devslopes-social/DataService.swift
@@ -10,14 +10,18 @@ import Foundation
 import Firebase
 
 let DB_BASE = Database.database().reference()
+let STORAGE_BASE = Storage.storage().reference()
 
 class DataService{
     static let ds = DataService()
-    
+    //DB references
     private var _REF_BASE = DB_BASE
     private var _REF_POSTS = DB_BASE.child("posts")
     private var _REF_USERS = DB_BASE.child("users")
 
+    // Storage references
+    private var _REF_POSTS_IMAGES = STORAGE_BASE.child("post-pics")
+    
     var REF_BASE: DatabaseReference{
         return _REF_BASE
     }
@@ -28,6 +32,10 @@ class DataService{
     var REF_USERS: DatabaseReference{
         return _REF_USERS
     }
+    var REF_POSTS_IMAGES: StorageReference{
+        return _REF_POSTS_IMAGES
+    }
+    
     func createFirebaseDBUser(uid: String, userData: Dictionary<String,String>){
         REF_USERS.child(uid).updateChildValues(userData)
     }
--- devslopes-social/PostCell.swift
+++ devslopes-social/PostCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class PostCell: UITableViewCell {
 
@@ -16,12 +17,35 @@ class PostCell: UITableViewCell {
     @IBOutlet weak var caption: UITextView!
     @IBOutlet weak var likeLbl: UILabel!
     
+    var post: Post!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
     }
-    func configureCell(post: Post){
+    func configureCell(post: Post, img: UIImage? = nil){
+        self.post = post
         self.caption.text = post.caption
         self.likeLbl.text = "\(post.likes)"
+        
+        if img != nil{
+            self.postImg.image = img
+        }else{
+            let ref = Storage.storage().reference(forURL: post.imageUrl)
+            ref.getData(maxSize: 2 * 1024 * 1024, completion: { (data, error) in
+                if error != nil{
+                    print("Unable to download image from Firebase Storage")
+                }else{
+                    print("Image downlaoding from Firebase storage")
+                    if let imgData = data{
+                        if let img = UIImage(data: imgData){
+                            self.postImg.image = img
+                            FeedVC.imageCache.setObject(img, forKey: post.imageUrl as! NSString)
+                        }
+                    }
+                }
+            })
+        }
+        
     }
 
 }
