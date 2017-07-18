06cf5e0255c4fb480e196f858a4cfb4d7692a666
xannas <wizwristy@gmail.com>
2017-07-18 15:03:49 +0700

    Completed getting data Firebase

--- devslopes-social/FeedVC.swift
+++ devslopes-social/FeedVC.swift
@@ -17,6 +17,10 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        
+        DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
+            print(snapshot.value)
+        })
     }
     
     @IBAction func signOutPressed(_ sender: Any) {
