e2b8b5afd7d77dc81a135e0f4d295b97f8ae8a62
xannas <wizwristy@gmail.com>
2017-07-18 13:35:05 +0700

    Complete feed table

--- devslopes-social/FeedVC.swift
+++ devslopes-social/FeedVC.swift
@@ -10,20 +10,34 @@ import UIKit
 import SwiftKeychainWrapper
 import Firebase
 
-class FeedVC: UIViewController {
-
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
+    
     @IBAction func signOutBtnPressed(_ sender: UIButton) {
         let keychainResult = KeychainWrapper.standard.removeObject(forKey: KEY_UID)
         print("ID remove from keychain -\(keychainResult)")
         try! Auth.auth().signOut()
         performSegue(withIdentifier: "goToSignIn", sender: nil)
     }
-
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        return cell
+    }
     
 
    
---
+++ devslopes-social/PostCell.swift
@@ -0,0 +1,26 @@
+//
+//  PostCell.swift
+//  devslopes-social
+//
+//  Created by xannas on 7/18/2560 BE.
+//  Copyright © 2560 xannas. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likeLbl: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+}
